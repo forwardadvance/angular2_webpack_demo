0e344553c109f8ac1996e3e10dc3f27a615cebf5
Nicholas Johnson <nicholas@forwardadvance.com>
2017-03-22 13:38:52 +0000

    webpack config

--- exercise/webpack.config.js
+++ exercise/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader' }
+      { test: /\.js$/, loader: 'babel-loader?presets[]=es2015' }
     ]
   }
 };
